3d496f85b171d51117627c48e48579654cac85c5
Eric-JG <guerrero.eric.j@gmail.com>
2021-07-29 20:27:47 -0400

    added code to attendance file

--- models/attendance.js
+++ models/attendance.js
@@ -1,8 +1,22 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 
+class Attendance extends Model {}
 
-
-
+Attendance.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  date: {
+    type: DataTypes.DATEONLY,
+    allowNull: true,
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+});
 
 module.exports = Attendance;
